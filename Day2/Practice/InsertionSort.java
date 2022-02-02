1b02c6f0c3ef465b2aa1c4951d8277d988c33a22
TweshaSatia <twesha.satia@searce.com>
2022-02-02 12:34:59 +0530

    Bubble sort bidirectional and Insertion sort to count copies and comparisons

--- Day2/Practice/InsertionSort.java
+++ Day2/Practice/InsertionSort.java
@@ -2,45 +2,60 @@ package Day2.Practice;
 //counts the number of copies and the number of comparisons it makes during a sort and
 //displays the totals.
 public class InsertionSort {
-    public static void insertionSort(int arr[]) { 
-    	int n = arr.length, count=0; 
-    
-    	count += 2; // 2op: i=1, i<n in loop start
-    	for (int i = 1; i < n; ++i) {
-    
-    		int backup = arr[i];    // +1 copy
-    		int j = i - 1;  // +1 assign
-    		count += 2;
-    
-            count+=1;   // +1 comp below
-    		while (j >= 0) {
-    			count+=1;   // +1 comp below
-    			if(arr[j] > backup) break;
-    
-    			arr[j + 1] = arr[j];    // +1 copy
-    			j = j - 1;  // +1 assign
-    			count += 2;
-    		}
-    		arr[j + 1] = backup;    // +1 copy
-    		count++;
-    		
-    		//  +1 assign: i++
-    		//  +1 comp: i<n (condition for next loop iteration)
-    		count += 2;
-    	}
-    	double N = arr.length;
-    	double N2 = Math.pow(N,2);
-    	System.out.println("Array size N:"+ N +"    N^2:"+ N2 +"    Count:"+ count);
-    }
-
-	public static void main(String args[]) 
-	{ 
-		int arr[] = {3, 4}; 
-		insertionSort(arr);
-		int arr2[] = {3, 4, 2, 9, 1, 6, 8, 13}; 
-		insertionSort(arr2);
-		int arr3[] = {3, 4, 2, 9, 1, 6, 8, 13, 3, 4, 2, 9, 1, 6, 8, 13, 3, 4, 2, 9, 1, 6, 8, 13, 3, 4, 2, 9, 1, 6, 8, 13}; 
-		insertionSort(arr3);
+    public static void printArray(int[] a)
+	{
+		int j;
+		for(j=0; j<a.length; j++)
+			System.out.print(a[j] + ", ");
+		System.out.println("\n");
+	}
+	
+	public static void insertionSort(int[] a)
+	{
+		int curr_item_index, insert_index, curr_item,comp=0,copy=0;
+		for (curr_item_index=1; curr_item_index<a.length; curr_item_index++)
+		{
+			
+			curr_item = a[curr_item_index];
+			for(insert_index=curr_item_index;insert_index>0;insert_index--)
+			{
+			 comp++;
+			if(curr_item<a[insert_index-1])
+			{
+				a[insert_index] = a[insert_index-1];
+				copy++;
+			}
+			else
+				break;
+			}	
+			a[insert_index] = curr_item;
+			System.out.println("Iteration " + (curr_item_index) + "==>");
+			
+			printArray(a);
+		}
+		System.out.println("Total Comparisons:"+comp);
+		System.out.println("Total Copies:"+copy);
+	}
+	public static void main(String[] args)
+	{
+		int[] array1 = {10,9,8,7,6,5,4};
+		int[] array = {0,-1,5,4,9,10};
+		System.out.println("Almost Sorted Array:");
+		System.out.println("Before Sorting===>");
+		printArray(array);
+		insertionSort(array);
+		System.out.println("After Sorting===>");
+		printArray(array);
+		
+		System.out.println("Inversely Sorted Array:");
+		System.out.println("Before Sorting===>");
+		printArray(array1);
+		insertionSort(array1);
+		System.out.println("After Sorting===>");
+		printArray(array1);
+		
+		//The efficiency of this algorithm for almost sorted data is less than O(N^2)
+		//For reverse array the comparisons and copies are almost same
 		
 	} 
 }
